e51b5f33266e2d45c495ca160dec275fc9deaa27
Robin Wilson <robin@rtwilson.com>
2019-10-20 11:59:20 +0100

    First commit

---
+++ get_petrol_prices.py
@@ -0,0 +1,71 @@
+import time
+import os
+
+import pandas as pd
+
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.keys import Keys
+
+
+def get_petrol_prices(email, password, fuel_type):
+    options = Options()
+    options.add_argument("no-sandbox")
+    options.add_argument("headless")
+    options.add_argument("start-maximized")
+    options.add_argument("window-size=1900,1080")
+
+    driver = webdriver.Chrome(options=options)
+
+    if fuel_type == 'unleaded':
+        fuel_type_id = 2
+    elif fuel_type == 'diesel':
+        fuel_type_id = 5
+
+    url = f"https://app.petrolprices.com/map?fuelType={fuel_type_id}&brandType=0&resultLimit=0&offset=0&sortType=price&lat=50.9317768&lng=-1.4436572&z=11&d=2"
+    driver.get(url)
+
+    email_field = driver.find_element_by_xpath('//*[@id="form-login"]/div[1]/input')
+    email_field.clear()
+    email_field.send_keys(email)   
+
+    password_field = driver.find_element_by_xpath('//*[@id="passwordInput"]')
+    password_field.clear()
+    password_field.send_keys(password)
+    password_field.send_keys(Keys.RETURN)
+    time.sleep(1)
+    try:
+        password_field.send_keys(Keys.RETURN)
+    except:
+        pass
+
+    driver.implicitly_wait(5)
+
+    station_boxes = driver.find_elements_by_class_name('stationDiv')
+
+    data = []
+
+    for station_box in station_boxes:
+        name = station_box.find_element_by_class_name('station-name').text[3:].strip()
+        price = station_box.find_element_by_class_name('station-price').text[:-1].strip()
+
+        if '-' in price:
+            continue
+
+        updated_date = station_box.find_element_by_xpath("div[@class='stationRightSide']/h5[1]").text.strip()
+
+        data.append({'name': name,
+                    'price': price,
+                    'updated_date': updated_date,
+                    'fuel_type': fuel_type})
+
+    df = pd.DataFrame(data)
+
+    return df
+
+
+email = os.environ['PETROL_PRICES_EMAIL']
+password = os.environ['PETROL_PRICES_PASSWORD']
+
+df = get_petrol_prices(email, password, 'unleaded')
+print(df)
